3e13809209d0018c8b9c4b2bb8eb50e90e43e9c1
francis-tg <franc9885@gmail.com>
2024-09-12 02:33:23 +0000

    add send me mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,150 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
-    function myMenuFunction(){
-      var menuBtn = document.getElementById("myNavMenu");
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
 
-      if(menuBtn.className === "nav-menu"){
-        menuBtn.className += " responsive";
-      } else {
-        menuBtn.className = "nav-menu";
-      }
-    }
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-    window.onscroll = function() {headerShadow()};
+window.onscroll = function () { headerShadow() };
 
-    function headerShadow() {
-      const navHeader =document.getElementById("header");
+function headerShadow() {
+  const navHeader = document.getElementById("header");
 
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 
-        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-        navHeader.style.height = "70px";
-        navHeader.style.lineHeight = "70px";
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
 
-      } else {
+  } else {
 
-        navHeader.style.boxShadow = "none";
-        navHeader.style.height = "90px";
-        navHeader.style.lineHeight = "90px";
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
 
-      }
-    }
+  }
+}
 
 
 /* ----- TYPING EFFECT ----- */
-   var typingEffect = new Typed(".typedText",{
-      strings : ["Developer","Designer","Analyst"],
-      loop : true,
-      typeSpeed : 100, 
-      backSpeed : 80,
-      backDelay : 2000
-   })
+var typingEffect = new Typed(".typedText", {
+  strings: ["Developer", "Designer", "Analyst"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+})
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-   const sr = ScrollReveal({
-          origin: 'top',
-          distance: '80px',
-          duration: 2000,
-          reset: true     
-   })
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
 
-  /* -- HOME -- */
-  sr.reveal('.featured-text-card',{})
-  sr.reveal('.featured-name',{delay: 100})
-  sr.reveal('.featured-text-info',{delay: 200})
-  sr.reveal('.featured-text-btn',{delay: 200})
-  sr.reveal('.social_icons',{delay: 200})
-  sr.reveal('.featured-image',{delay: 300})
-  
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.social_icons', { delay: 200 })
+sr.reveal('.featured-image', { delay: 300 })
 
-  /* -- PROJECT BOX -- */
-  sr.reveal('.project-box',{interval: 200})
 
-  /* -- HEADINGS -- */
-  sr.reveal('.top-header',{})
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box', { interval: 200 })
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {})
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  const srLeft = ScrollReveal({
-    origin: 'left',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srLeft.reveal('.about-info',{delay: 100})
-  srLeft.reveal('.contact-info',{delay: 100})
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
 
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  const srRight = ScrollReveal({
-    origin: 'right',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srRight.reveal('.skills-box',{delay: 100})
-  srRight.reveal('.form-control',{delay: 100})
-  
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.contact-info', { delay: 100 })
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+
 
 
 /* ----- CHANGE ACTIVE LINK ----- */
-  
-  const sections = document.querySelectorAll('section[id]')
 
-  function scrollActive() {
-    const scrollY = window.scrollY;
+const sections = document.querySelectorAll('section[id]')
 
-    sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 50,
-        sectionId = current.getAttribute('id')
+function scrollActive() {
+  const scrollY = window.scrollY;
 
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
 
-          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
 
-      }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    } else {
 
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+function linkToSendMail({ name, email, message }) {
+  const link = document.createElement("a");
+  const linkValue = `mailto:jordannono2245@gmail.com?subject=${name}&body=${message}`
+  link.setAttribute("href", linkValue)
+  link.click();
+}
+
+const formsToValidate = document.querySelectorAll('form[validate]');
+formsToValidate.forEach((form) => {
+  const data = {}
+  form.querySelector("button").addEventListener("click", function (e) {
+    const inputs = [...form.querySelectorAll('input'), ...form.querySelectorAll("select"), ...form.querySelectorAll("textarea")];
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+      if (input.value !== "") {
+        data[input.name ?? input.id] = input.value
+        input.style.borderColor = "gray"
+        console.log(data)
+        e.preventDefault()
+
+
+      } else {
+        e.preventDefault()
+        input.style.borderColor = "red"
       }
-    })
-  }
-
-  window.addEventListener('scroll', scrollActive)
+    }
+    linkToSendMail(data)
+    setTimeout(() => {
+      e.stopPropagation()
+    }, 1000);
+  })
+})
